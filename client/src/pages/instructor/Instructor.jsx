e903ce02b09e28660699b04b0809546dcd5339ff
OGtrilliams <tribecca@tribecc.us>
2025-10-23 18:54:40 -0400

    corrected typos in webhooks

--- client/src/pages/instructor/Instructor.jsx
+++ client/src/pages/instructor/Instructor.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../../components/instructor/Navbar.jsx";
 import Sidebar from "../../components/instructor/Sidebar.jsx";
 import Footer from "../../components/instructor/Footer.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 const Instructor = () => {
   return (
@@ -9,7 +10,10 @@ const Instructor = () => {
       <Navbar />
       <div className="flex">
         <Sidebar />
-        <div className="flex-1">{<Outlet />}</div>
+        <div className="flex-1">
+          {<Outlet />}
+          <Analytics />{" "}
+        </div>
       </div>
       <Footer />
     </div>
